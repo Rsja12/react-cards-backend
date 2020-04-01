d1e1628e101ab3de13a07c6990964eff379471f5
Rsja12 <asocarras10@gmail.com>
2020-04-01 18:54:25 -0400

    fixes create action.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
     def create
         user = User
-        .find_by(email: params["users"]["email"])
+        .find_by(email: params["user"]["email"])
         .try(:authenticate, params["user"]["password"])
 
         if user 
