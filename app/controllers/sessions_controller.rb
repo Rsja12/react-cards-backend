2da7337d87cfeb575946f6fb046a3e43f2aac66d
Rsja12 <asocarras10@gmail.com>
2020-04-01 19:05:10 -0400

    adds logout action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -31,5 +31,10 @@ class SessionsController < ApplicationController
             }
         end
     end
+
+    def logout
+        reset_session
+        render json: { status: 200, logged_out: true }
+    end
     
 end
